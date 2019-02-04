6f6877f9c18ac7b7e906d0e008f98d6f84859f01
Joshua Ruelas <ruelastech7@gmail.com>
2019-02-03 20:10:57 -0800

    docs: Added web server source file

---
+++ webserver.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/api/<string:name>/<int:id>/')
+def JsonHandler(name, id):
+	return jsonify(name=name, id=id)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
